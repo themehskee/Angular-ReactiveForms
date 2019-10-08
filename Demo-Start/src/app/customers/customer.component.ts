b78ee2eb1f92b81d79bc6559256175d165de65fb
Cvetan Dimeski <cdimeski@CDIMESKIDTOP.HQ>
2019-10-08 11:26:58 +0200

    added two new form controls about phone number and notifications

--- Demo-Start/src/app/customers/customer.component.ts
+++ Demo-Start/src/app/customers/customer.component.ts
@@ -19,6 +19,8 @@ export class CustomerComponent implements OnInit {
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
       email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      notification: 'email',
       sendCatalog: true
     });
   }
